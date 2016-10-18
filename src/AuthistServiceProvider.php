75acc54e3693a96fffdeec4f258766e1fa859c02
Eko Eryanto <ekoeryanto@gmail.com>
2016-10-18 15:03:06 +0700

    add service provider

--- src/AuthistServiceProvider.php
+++ src/AuthistServiceProvider.php
@@ -1,15 +1,35 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: nueko
- * Date: 10/18/2016
- * Time: 3:01 AM
- */
 
 namespace Authist;
 
+use Illuminate\Support\ServiceProvider;
 
-class Authize
+class AuthistServiceProvider extends ServiceProvider
 {
 
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'authist');
+
+        $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'authist');
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../resources/views' => resource_path('views/vendor/authist'),
+            ], 'authist-views');
+
+            $this->publishes([
+                __DIR__ . '/../database/migrations' => database_path('migrations'),
+            ], 'authist-migrations');
+
+            $this->publishes([
+                __DIR__ . '/../resources/lang' => resource_path('lang/vendor/authist'),
+            ], 'authist-locale');
+        }
+    }
+
+    public function register()
+    {
+        //
+    }
 }
